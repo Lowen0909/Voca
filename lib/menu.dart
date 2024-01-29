2d31b3b43970f09e4dd80143423626afa60b2b5e
Lowen0909 <eddie206027@gmail.com>
2024-01-29 16:08:06 +0800

    Add a function to the program that can read JSON files.

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'database/dict.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:open_file/open_file.dart';
+import 'dart:io';
+import 'dart:convert';
+
 class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
    // 把MyHomePage設為一個StatefulWidget
@@ -30,11 +35,16 @@ class _MenuPage extends State<MenuPage> {
       _selectedIndex = index;
     });
   }
+  final List<Widget> _pages = [
+    Home(),
+    Test(),
+    MyFile(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: _widgetOptions.elementAt(_selectedIndex),
+          child: _pages[_selectedIndex],
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
@@ -55,6 +65,68 @@ class _MenuPage extends State<MenuPage> {
           selectedItemColor: Colors.amber[800],
           onTap: _onItemTapped,
         ),
+    );
+  }
+}
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+        'Home page'
+      ),
+      ),
+    );
+  }
+}
+class Test extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+            'Test page'
+        ),
+      ),
+    );
+  }
+}
+class MyFile extends StatelessWidget{
+  void _openFile(PlatformFile file) {
+    OpenFile.open(file.path);
+  }
+  Future<void> loadJson(PlatformFile Myfile) async {
+    final String jsonString = await File(Myfile.path.toString()).readAsString();
+    List<Map<String,dynamic>> data = List.from(json.decode(jsonString) as List);;
+    print(data);
+  }
+
+  void _pickFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false,type: FileType.custom,
+      allowedExtensions: ['json'],);
+
+    // if no file is picked
+    if (result == null) return;
+    final file = result.files.first;
+    loadJson(file);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            _pickFile();
+          },
+          child: Text(
+            'Open file',
+            style: TextStyle(color: Colors.white),
+          ),
+          color: Colors.green,
+        ),
+      ),
     );
   }
 }
